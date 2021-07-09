1cf22103a631a5afe5a855957c637f33cbd94743
Andrzej Jończy <72267678+andree0@users.noreply.github.com>
2021-07-09 12:46:32 +0200

    Update FizzBuzz.py

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,7 +1,7 @@
 result = []
 for i in range(1, 101):
     result.append("Fizz"*(i % 3 == 0) + "Buzz"*(i % 5 == 0) or i)
-#     result.append([i, "Buzz", "Fizz", "FizzBuzz"][2*(i % 3 == 0) + (i % 5 == 0)]
+#     result.append([i, "Buzz", "Fizz", "FizzBuzz"][2*(i % 3 == 0) + (i % 5 == 0)])
 
 print(result)
 
